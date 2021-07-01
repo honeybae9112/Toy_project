4b9d215b40b86db5202852bacf348c14ef8e8622
honeybae9112 <nike999111@naver.com>
2021-07-02 03:25:20 +0900

    [product] 범위 값 목록 조회 수정

--- src/main/java/com/honeybae/project/service/ProductService.java
+++ src/main/java/com/honeybae/project/service/ProductService.java
@@ -2,6 +2,8 @@ package com.honeybae.project.service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,33 +23,47 @@ public class ProductService  {
 
 	public List<Product> selectList(Search searchDto) throws Exception {
 		// 조건1
-		// price 20000~50000 의 리스트 리턴 
+		// price 최소값 최대값 사이의 리스트 리턴 
 		int minPrice = searchDto.getMinPrice();
 		int maxPrice = searchDto.getMaxPrice();
+		String brand = searchDto.getBrand();
 		
-		if(minPrice ==0 && maxPrice==0) {
+		if(minPrice ==0 && maxPrice==0 && brand == null) {
 			return productRepository.selectList();
 		}else {
 			// case1. 전체 목록을 조회해서 순차적으로 비교한다
-			List<Product> respList = new ArrayList<>();
-			for(Product dbData : productRepository.selectList()) {
-				if(maxPrice == 0) {
-					if(dbData.getPrice()>=minPrice) {
-						respList.add(dbData);
-					}
-				}else if(minPrice == 0) {
-					if(dbData.getPrice()<=maxPrice) {
-						respList.add(dbData);
-					}
-				}
-				else if(maxPrice>minPrice) {
-					if(dbData.getPrice()>=minPrice
-							&& dbData.getPrice()<=maxPrice) {
-						respList.add(dbData);
-					}
-				}
+//			List<Product> respList = new ArrayList<>();
+//			for(Product dbData : productRepository.selectList()) {
+//				if(maxPrice == 0
+//						&& dbData.getPrice()>=minPrice) {
+//						respList.add(dbData);
+//				}else if(minPrice == 0
+//							&& dbData.getPrice()<=maxPrice) {
+//						respList.add(dbData);
+//				}
+//				else if(dbData.getPrice()>=minPrice
+//							&& dbData.getPrice()<=maxPrice) {
+//						respList.add(dbData);
+//				}
+//			}
+//			return respList;
+			// case2. 반복자 이용 조건값 불일치값 제거 
+			List<Product>productList = productRepository.selectList();
+			Iterator<Product> itr = productList.iterator();
+			while (itr.hasNext()) {
+				Product dbData = itr.next();
+				if( minPrice > 0 ? dbData.getPrice() < minPrice : false ) {
+					itr.remove();
+					continue;
+				}else if( maxPrice > 0 ? dbData.getPrice() > maxPrice : false ) {
+					itr.remove();
+					continue;
+				}else if( brand != null ? !dbData.getBrand().equals(brand):false) {
+					itr.remove();
+					continue;
+				} 
 			}
-			return respList;
+			return productList;
 		}
 	}
 
