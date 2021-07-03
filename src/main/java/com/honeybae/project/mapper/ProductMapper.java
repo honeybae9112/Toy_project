4ca1df5733dc2f30d8b004a46292be2db6995e9d
honeybae9112 <nike999111@naver.com>
2021-07-03 17:18:21 +0900

    [Product] memory -> DB 로 변경

--- src/main/java/com/honeybae/project/mapper/ProductMapper.java
+++ src/main/java/com/honeybae/project/mapper/ProductMapper.java
@@ -2,16 +2,19 @@ package com.honeybae.project.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
 import com.honeybae.project.dto.Product;
-//@Mapper
-//@Repository
+@Repository
+@Mapper
 public interface ProductMapper {
 	
 	public List<Product> selectList(Product dto)throws Exception;
 	public Product selectGet(Product dto);
-	public void productAdd(Product dto);
-	public void productUpdate(Product dto);
-	public void productDelete(Product dto);
+	public void addProduct(Product dto);
+	public void updateProduct(Product dto);
+	public void deleteProduct(Product dto);
 	public void delete(Product dto);
 	public int count(Product dto);
 	public void readCount(Product dto);
