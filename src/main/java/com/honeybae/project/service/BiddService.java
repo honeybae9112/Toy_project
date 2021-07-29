24b1b35285e3762a4a7caa75ab0af6b3224f1338
honeybae9112 <nike999111@naver.com>
2021-07-30 00:35:25 +0900

    [bidd] 입찰 시 상한가 하한가 케이스 반영

--- src/main/java/com/honeybae/project/service/BiddService.java
+++ src/main/java/com/honeybae/project/service/BiddService.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.honeybae.project.dto.Auction;
-import com.honeybae.project.dto.AuctionVO;
 import com.honeybae.project.dto.Bidd;
-import com.honeybae.project.dto.BiddVO;
 import com.honeybae.project.mapper.AuctionMapper;
 import com.honeybae.project.mapper.BiddMapper;
 import com.honeybae.project.util.AuctionState;
@@ -27,49 +25,60 @@ public class BiddService extends AuctionState {
 	public void add(int auctionId,Bidd dto) {
 		int biddPrice = dto.getBiddPrice();
 		int bidderId = dto.getBidderId();
-		AuctionVO auctionVo = auctionMapper.select(auctionId);
-		// 경매중 시 입찰
-		if(auctionVo.getAuctionState()==START) {
-			// 나의 입찰내역을 조회 한뒤
-			// 입찰한 기록이 존재하면 입찰가와 날짜를 수정하고
-			// 없으면 추가한다
-			BiddVO biddVo = select(auctionId,bidderId);
-			if(biddVo != null) {
-				int biddId = biddVo.getId();
-				update(biddId,biddPrice);
-			}else {
-				dto.setBiddDate(TimeUtil.getDate(new Date(),"yyyy-MM-dd HH:mm:ss"));
-				biddMapper.add(dto);
-			}
-			// 현재가보다 입찰가가 높은 경우 경매에 현재가격을 업데이트한다
-			int currentPrice = auctionVo.getCurrentPrice();
-			//여러명 동시에 동일 가격 입찰시 가격이 같으면 ?
-			if(currentPrice < biddPrice) {
-				Auction auctionDto = new Auction();
-				auctionDto.setId(auctionId);
-				auctionDto.setCurrentPrice(biddPrice);
-				auctionMapper.updateByPrice(auctionDto);
-			}else {
-				// 현재가 보다 작으면?
-			}
-		// 진행중이 아니면?
-		}else {
+		Auction auction = auctionMapper.select(auctionId);
+		Integer auctionMaxPrice = auction.getMaxPrice();
+		Integer auctionMinPrice = auction.getMinPrice();
+		int currentPrice = auction.getCurrentPrice();
+		dto.setBiddDate(TimeUtil.getDate(new Date(),"yyyy-MM-dd HH:mm:ss"));
 
+		if(auction.getAuctionState()==START) {
+			Bidd bidd = select(auctionId,bidderId);
+			if(bidd != null) {
+				int biddId = bidd.getId();
+				if(auctionMinPrice != null && auctionMaxPrice != null) {
+					if(biddPrice>auctionMinPrice && biddPrice<auctionMaxPrice) {
+						update(biddId,biddPrice);
+						updateAuctionPrice(auctionId,currentPrice,biddPrice);
+					}
+				}else {
+					update(biddId,biddPrice);
+					updateAuctionPrice(auctionId,currentPrice,biddPrice);
+				}
+			}else {
+				if(auctionMinPrice != null && auctionMaxPrice != null) {
+					if(biddPrice>auctionMinPrice && biddPrice<auctionMaxPrice) {
+						biddMapper.add(dto);
+						updateAuctionPrice(auctionId,currentPrice,biddPrice);
+					}
+				}else {
+					biddMapper.add(dto);
+					updateAuctionPrice(auctionId,currentPrice,biddPrice);
+				}
+			}
 		}
 	}
-	public BiddVO select(int auctionId, int bidderId) {
+	private void updateAuctionPrice(int auctionId,int currentPrice,int biddPrice) {
+		if(biddPrice>currentPrice) {
+			Auction auctionDto = new Auction();
+			auctionDto.setId(auctionId);
+			auctionDto.setCurrentPrice(biddPrice);
+			auctionMapper.updateByPrice(auctionDto);
+		}
+	}
+
+	public Bidd select(int auctionId, int bidderId) {
 		Bidd biddDto = new Bidd();
 		biddDto.setAuctionId(auctionId);
 		biddDto.setBidderId(bidderId);
 		return biddMapper.select(biddDto);
 	}
-	public BiddVO select(int biddId) {
+	public Bidd select(int biddId) {
 		return biddMapper.select(biddId);
 	}
-	public List<BiddVO> selectList(int bidderId){
+	public List<Bidd> selectList(int bidderId){
 		return biddMapper.selectList(bidderId);
 	}
-	public List<BiddVO> selectListAuction(int auctionId) {
+	public List<Bidd> selectListAuction(int auctionId) {
 		return biddMapper.selectListByAuction(auctionId);
 	}
 	public void update(int biddId,int biddPrice) {
@@ -80,7 +89,21 @@ public class BiddService extends AuctionState {
 		biddMapper.update(biddDto);
 	}
 	public void delete(int biddId) {
+		Bidd bidd = select(biddId);
+		int bidderPrice = bidd.getBiddPrice();
+		int auctionId = bidd.getAuctionId();
+
+		Auction auction = auctionMapper.select(auctionId);
+		int currentPrice = auction.getCurrentPrice();
+
 		biddMapper.delete(biddId);
+		if(bidderPrice == currentPrice) {
+			int biddMaxPrice = biddMapper.selectMaxPrice(auctionId);
+			Auction auctionDto = new Auction();
+			auctionDto.setId(auctionId);
+			auctionDto.setCurrentPrice(biddMaxPrice);
+			auctionMapper.updateByPrice(auctionDto);
+		}
 	}
 	public void deleteAuction(int auctionId) {
 		biddMapper.deleteByAuction(auctionId);
